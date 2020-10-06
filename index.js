82d703abd7f06a14ead329ed076dfadcd376f446
loveyousomuch <loveyousomuch554@gmail.com>
2020-10-06 16:31:35 +0300

    processing 404 page

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.use(express.static('public'))
+
+// Processing 404 page
+app.use((req, res, next) => {
+  let p = path.join(__dirname, 'public', '404.html')
+
+  res.status(404).sendFile(p)
+})
 
 app.listen(8080)
