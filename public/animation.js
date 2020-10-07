b13600b7f66a7630425fee399dd46d16284be999
loveyousomuch <loveyousomuch554@gmail.com>
2020-10-07 15:08:09 +0300

    page animation

---
+++ public/animation.js
@@ -0,0 +1,42 @@
+window.onload = startAnimations
+
+function startAnimations() {
+  document.body.style.opacity = '1'
+
+  // Main divs
+  Im_Nikita.style.top = '124px'
+  Aspiring__software_engineer.style.top = '223px'
+
+  // Main svg
+  main_page.style.top = '-100px'
+
+  // Icons on the top page
+  main_tw.style.top = '463px'
+  main_inst.style.top = '459px'
+  main_git.style.top = '459px'
+
+  // About my experience
+  about_me.style.top = '700px'
+
+  // Svg near about my experience 
+  about_my_path_svg.style.top = '805px'
+
+  // Skills
+  skills.style.top = '1300px'
+
+  // Svg near skills 
+  window_about_skills.style.top = '1400px'
+
+  // what_i_worked_on
+  what_i_worked_on.style.top = '1725px'
+
+  // Svg what_i_worked_on
+  projects.style.top = '1800px'
+
+  // Links
+  info_links.style.top = '2450px'
+
+  // Bottom svgs
+  social_svg.style.top = '2582px'
+  window_about_links.style.top = '2782px'
+}
