d69826d337551ca82ad98bb5ac91b0057ecb313d
loveyousomuch <loveyousomuch554@gmail.com>
2020-10-13 01:51:04 +0300

    env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const MobileDetect = require('mobile-detect')
+require('dotenv').config()
 
 app.use((req, res, next) => {
   let md = new MobileDetect(req.get('user-agent'))
@@ -26,4 +27,8 @@ app.use((req, res, next) => {
   res.status(404).sendFile(p)
 })
 
-app.listen(8080)
+const PORT = process.env.NODE_ENV === "production" ?
+  process.env.PORT :
+  8080;
+
+app.listen(PORT)
