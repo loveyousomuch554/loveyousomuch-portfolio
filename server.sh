dd102d80bb49004c389a62631d765d67e5aefa39
loveyousomuch <loveyousomuch554@gmail.com>
2020-10-14 23:02:36 +0300

    added server management

--- server.sh
+++ server.sh
@@ -8,14 +8,19 @@ then
 fi
 
 if [ $command == 'start' ]
-then
-    echo "command is start"
+then    
+    # Star npm script
+    cd /opt/bitnami/apps/portfolio
+    sudo npm run server
 elif [ $command == 'stop' ]
 then
-    echo "command is stop"
+    # Kill process on port 443 
+    sudo kill -9 $(sudo lsof -t -i:443)
 elif [ $command == 'restart' ]
-then
-    echo "restarting server"
+then    
+    cd /opt/bitnami/apps/portfolio
+    sudo kill -9 $(sudo lsof -t -i:443)
+    sudo npm run server
 else
     echo "invalid command"
 fi
