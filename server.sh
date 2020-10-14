5b509cff005cfcb53f09f73262d345bbda72cff0
loveyousomuch <loveyousomuch554@gmail.com>
2020-10-14 19:46:39 +0300

    shell script for server management

---
+++ server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+command=$1
+
+# If input is empty set the variable to null
+if [ -z "$command" ]
+then
+      command="null"
+fi
+
+if [ $command == 'start' ]
+then
+    echo "command is start"
+elif [ $command == 'stop' ]
+then
+    echo "command is stop"
+elif [ $command == 'restart' ]
+then
+    echo "restarting server"
+else
+    echo "invalid command"
+fi
