bdf902af8fc8478d3a82c9bb72fa3d778b29f138
loveyousomuch <loveyousomuch554@gmail.com>
2020-10-19 15:27:40 +0300

    proxy

--- routes/projects.js
+++ routes/projects.js
@@ -4,8 +4,13 @@ const { routes: project_routes } = require('../reverseproxyconfig.json')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 // https://loveyousomuch554.dev/projects/
 
-for({route, address} of project_routes) {
-  Router.use(route, createProxyMiddleware({ target: address, changeOrigin: true }));
+for({route, address} of project_routes) { 
+  Router.use(route, createProxyMiddleware({ 
+    target: address, 
+    pathRewrite: (path, req) => { 
+      return path.split('/').slice(2).join('/') 
+    } 
+  }))
 }
 
 exports.Router = Router
