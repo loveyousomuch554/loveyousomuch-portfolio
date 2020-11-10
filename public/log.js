43c3f3fea7c41c8d5362831dcedfca3dd555614c
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-10 14:32:25 +0300

    logs

--- public/log.js
+++ public/log.js
@@ -1,7 +1,16 @@
-console.log('%cWhat are you doing here?', 'color:#645394; font-size: 20px; font-family: Source Code Pro;')
-console.log('%cMy mail, just in case <3', 'color:#645394; font-size: 20px; font-family: Nunito Sans;')
-console.log('%cloveyousomuch554@gmail.com', `
-  color:#645394; 
-  font-size: 30px; 
+console.group('%cMy contacts, just in case <3', 'color: #645394; font-size: 18px; font-family: Nunito Sans;')
+console.log('%cloveyousomuch554@gmail.com', 'font-size: 20px; font-family: Nunito Sans;')
+console.log('%ctwitter:\nhttps://twitter.com/_love_yousomuch', 'color: #645394; font-size: 18px; font-family: Nunito Sans;')
+console.log('%ctelegram\nhttps://telegram.me/loveyousomuch554', 'color: #645394; font-size: 18px; font-family: Nunito Sans;')
+console.log('%cGitHub\nhttps://github.com/loveyousomuch554', 'color: #645394; font-size: 18px; font-family: Nunito Sans;')
+console.groupEnd()
+
+const space = ' '.repeat(15)
+console.log(`%c<3${space}<3${space}<3${space}`, `
+  color: #645394; 
+  font-size: 200px; 
   font-family: Nunito Sans;
+  background: url('http://localhost/static/log_wallpaper.png');
+  background-size: cover;
+  background-repeat: no-repeat;
 `)
