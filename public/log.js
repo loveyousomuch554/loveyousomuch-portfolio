858489ea7c370cb1aa13a3e915a2a3ff1e253591
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-10 11:38:28 +0300

    console logs

---
+++ public/log.js
@@ -0,0 +1,7 @@
+console.log('%cWhat are you doing here?', 'color:#645394; font-size: 20px; font-family: Source Code Pro;')
+console.log('%cMy mail, just in case <3', 'color:#645394; font-size: 20px; font-family: Nunito Sans;')
+console.log('%cloveyousomuch554@gmail.com', `
+  color:#645394; 
+  font-size: 30px; 
+  font-family: Nunito Sans;
+`)
